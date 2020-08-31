9624ac01abdd1f8c6a5745a847ade1a31f9c815a
Hugueds <hugo-pdf@hotmail.com>
2020-08-30 23:42:15 -0300

    add without recursion

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import numpy as np
 import numpy.ma as ma
 import matplotlib.pyplot as plt
 
+# WITH RECURSION
+
 MAX_GENERATIONS = 999
 
 target = 100
@@ -11,15 +13,12 @@ mutation_rate = 0.01
 global_best = []
 generation = 0
 
-y1 = np.zeros((elements), np.uint8)
-y2 = np.ones((elements), np.uint8)
-y = np.concatenate((y1,y2))
 
 def fitness(gene, target):
     sum_ = gene.sum()
     if sum_ > target:
-        return target / sum_
-    return sum_ / target
+        return pow(target / sum_, 2)
+    return pow(sum_ / target, 2)
 
 def generate_population(population, target, elements):
     gen = []
@@ -28,17 +27,28 @@ def generate_population(population, target, elements):
         gen.append([g, 0.0])
     return gen
 
-def crossover():
-    pass
+def crossover(parents):
+    global elements
+    y1 = np.zeros((elements), np.uint8)
+    y2 = np.ones((elements), np.uint8)
+    y = np.concatenate((y1,y2))
+    np.random.shuffle(y)        
+    child = ma.masked_array(parents, mask=y).compressed()
+    return child
 
-def mutate():
-    pass 
+def mutate(child):            
+    for i, g in enumerate(child):            
+        if mutation_rate > np.random.rand():
+            print('Mutation has ocurred')
+            child[i] = np.random.randint(1, 100)
+    return child
 
 def run(generation, current_members):
 
     global MAX_GENERATIONS, mutation_rate, global_best, y    
 
     if generation > MAX_GENERATIONS:
+        print(f'Max generation exceeded')
         print(current_members[0][0], current_members[0][0].sum())
         return generation
     
@@ -55,41 +65,33 @@ def run(generation, current_members):
             best2 = [gene[0], score]
         scores.append(score)
 
-    arr_scores = np.array(scores)
-    print(arr_scores.mean())
+    print(f'Generation Mean: {np.array(scores).mean()}')
     global_best.append(best1[1])
 
     if best1[1] == 1.0:
-        print(best1[0])
         print(f'You made it in gen: {generation}')
+        print(best1[0])
         return generation
 
+    parents = np.concatenate((best1[0],best2[0]))
     children = [best1, best2]    
 
-    c = np.concatenate((best1[0],best2[0]))
-
-    for i in range(elements - 2):        
-        np.random.shuffle(y)        
-        child = ma.masked_array(c, mask=y).compressed()        
-        for i, g in enumerate(child):            
-            if mutation_rate > np.random.rand():
-                print('Mutation has ocurred')
-                child[i] = np.random.randint(1, 100)
-        children.append([child, 0.0])
+    for i in range(elements - 2):
+        child = crossover(parents)
+        child = mutate(child)
+        children.append([child, 0.0])     
 
     current_members = children
-    generation += 1    
+    generation += 1
+
     return run(generation, current_members)
 
 current_members = generate_population(population, target, elements)
 
-final_gen = run(generation, current_members)
+final_gen = run(generation, current_members) 
 
- 
-
-
+# Plot the evolution
 x = np.arange(0,final_gen + 1)
-y_ = np.array(global_best)
-plt.plot(x, y_)
+y = np.array(global_best)
+plt.plot(x, y)
 plt.show()
-    
